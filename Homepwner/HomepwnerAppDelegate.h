f292bcbaedaaa493a59129c007c12d746ad2560e
Antonio Haley <antonio.haley@gmail.com>
2012-08-14 19:31:55 -0400

    Initial Commit

---
+++ Homepwner/HomepwnerAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HomepwnerAppDelegate.h
+//  Homepwner
+//
+//  Created by Antonio Haley on 8/14/12.
+//  Copyright (c) 2012 CBM of America. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomepwnerAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
